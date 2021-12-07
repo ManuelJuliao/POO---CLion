9b5cb7592ed129f9b629ba08f0fd5885e3937735
ManuelJuliao <38535154+ManuelJuliao@users.noreply.github.com>
2021-12-07 16:42:10 +0000

    V1

---
+++ POO/cmake-build-debug/CMakeFiles/POO.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/POO.dir/ilha.cpp.obj"
+  "CMakeFiles/POO.dir/interface.cpp.obj"
+  "CMakeFiles/POO.dir/jogada.cpp.obj"
+  "CMakeFiles/POO.dir/main.cpp.obj"
+  "POO.exe"
+  "POO.exe.manifest"
+  "POO.pdb"
+  "libPOO.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/POO.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
